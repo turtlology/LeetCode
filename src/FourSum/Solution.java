4a176a761852765e14c3814840514eee2f1d5c05
turtlologist <nxhejiabei@hotmail.com>
2016-09-12 11:13:45 +0800

    add 3 solutions

---
+++ src/FourSum/Solution.java
@@ -0,0 +1,12 @@
+package FourSum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+	public List<List<Integer>> fourSum(int[] nums, int target) {
+        ArrayList result = new ArrayList();
+        
+        return result;
+    }
+}
---
+++ src/LongestPalindromicSubstring/Solution.java
@@ -0,0 +1,11 @@
+package LongestPalindromicSubstring;
+
+public class Solution {
+    public String longestPalindrome(String s) {
+        if(s.length()<=1){
+        	return s;
+        }
+        String result = "";
+        return result;
+    }
+}
